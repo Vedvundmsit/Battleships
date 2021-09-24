a28776002d8c5e4e5139233e0b89e9337d83cfc4
Vedavyas Vundyala <vedvund@msitprogram.net>
2021-09-24 15:20:09 +0530

    addShips function is done.

--- battleship.py
+++ battleship.py
@@ -109,7 +109,15 @@ Parameters: 2D list of ints ; int
 Returns: 2D list of ints
 '''
 def addShips(grid, numShips):
-    return
+    board=grid
+    count=0
+    while count < numShips:
+        ship=createShip()
+        if checkShip(board, ship) == True:
+            for i in range(len(ship)):
+                board[ship[i][0]][ship[i][1]] = SHIP_UNCLICKED
+            count +=1
+    return board
 
 
 '''
@@ -288,7 +296,7 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testCheckShip()
+    test.testAddShips()
 
     ## Finally, run the simulation to test it manually ##
     # runSimulation(500, 500)
